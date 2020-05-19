5eef653369139ddf11153274a606ec2e27ca8127
Funk2256 <37512659+Funk2256@users.noreply.github.com>
2020-05-20 00:35:23 +0700

    Update Jaden Casing Strings.py

--- Jaden Casing Strings.py
+++ Jaden Casing Strings.py
@@ -9,17 +9,9 @@ def to_jaden_case(string):
     my_str = delimiter.join(lis_clear)
     return my_str
 
-"""to_jaden_case("If There Is Bread Winners, There Is Bread Losers. But You Can'T Toast What Isn'T Real." 
-should equal "If There Is Bread Winners, There Is Bread Losers. But You Can't Toast What Isn't Real.")"""
-
 string = "If There Is Bread Winners, There Is Bread Losers. But You Can'T Toast What Isn'T Real."
-
 to_jaden_case(string)
 
-"""b = "If There Is Bread Winners, There Is Bread Losers. But You Can't Toast What Isn't Real."
-
-#print(a)
-#print(b)
 if a == b:
     print('True')
 else:
