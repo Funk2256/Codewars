f7ad2216fc22db9780375f03367d5403ef7393ff
Funk2256 <funk2256@gmail.com>
2021-02-16 17:34:50 +0700

    new cata Divide and Conquer

---
+++ Divide and Conquer.py
@@ -0,0 +1,16 @@
+def div_con(x):
+    str_list = []
+    num_list = []
+    for item in x:
+        if isinstance(item, str):
+            str_list.append(int(item))
+        else:
+            num_list.append(int(item))
+
+    return (sum(num_list)-sum(str_list))
+
+
+""""
+def div_con(lst):
+    return sum(n if isinstance(n, int) else -int(n) for n in lst) - not my result
+"""
